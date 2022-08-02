904d7815b1834a34c8acfec7ef6fddd35d0635bf
Awais <64607461+Leostar2054@users.noreply.github.com>
2022-08-02 15:27:10 +0900

    Add files via upload
    MATLAB Basics

---
+++ Lesson_1.m
@@ -0,0 +1,23 @@
+
+%% Question 1.....MATLAB as Calculator
+
+principal = 5000;
+alpha = 0.15;
+debt = principal * (1 + alpha) ^ 2
+%disp(debt);
+
+
+%% Lesson 1 Wrap up
+
+time = 9.58 / 3600;
+distance = 0.1;
+hundred = distance / time
+
+distance = 42.195;
+time = 2 + 1 / 60 + 39 / 3600;
+marathon = distance / time
+
+
+%% Test Plot
+
+plot([1, 2, 3], [3, 5, 10], 'rs');
